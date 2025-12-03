fc787a88bee21fcf8295bacaeefc3765066e8617
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2025-12-03 22:35:13 +0700

    update

--- app/Http/Controllers/Vendor/InvoiceController.php
+++ app/Http/Controllers/Vendor/InvoiceController.php
@@ -187,7 +187,7 @@ class InvoiceController extends Controller
         $minimalInvoices = $vendorInvoices->getCollection()->map(function ($invoice) {
             return [
                 'invoice_number' => $invoice->invoice_number,
-                'date' => $invoice->created_at->format('d M Y'),
+                'date' => $invoice->created_at->format('y-m-d'),
                 'status' => ucfirst($invoice->status),
             ];
         });
