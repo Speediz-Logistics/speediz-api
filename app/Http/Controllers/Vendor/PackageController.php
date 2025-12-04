4cc3900fa60f7863580225f4b0354e85d2dba342
mekmun-sopheaktra <mekmunsopheaktra@gmail.com>
2025-12-04 18:12:47 +0900

    Update

--- app/Http/Controllers/Vendor/PackageController.php
+++ app/Http/Controllers/Vendor/PackageController.php
@@ -170,7 +170,7 @@ class PackageController extends Controller
     public function store(Request $request)
     {
         $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
+            'name' => 'nullable|string|max:255',
             'slug' => 'required|string',
             'price' => 'required|numeric|min:0',
             'description' => 'nullable|string',
