cdd4262b2000533bb802df3cadbb911337fd31cb
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2026-01-05 16:56:39 +0700

    Update

--- database/migrations/2026_01_05_110913_create_password_reset_tokens_table.php
+++ database/migrations/2026_01_05_110913_create_password_reset_tokens_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('email')->primary();
             $table->string('token');
-            $table->timestamps();
+            $table->timestamp('created_at')->nullable();
         });
     }
 
